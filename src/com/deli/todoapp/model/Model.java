095ee5543fe3568a6c4f27f76a424f38b8f6cd20
DaniyarZh <den-gak@mail.ru>
2017-05-05 22:44:39 +0400

    Added Model

--- src/com/deli/todoapp/model/Model.java
+++ src/com/deli/todoapp/model/Model.java
@@ -1,7 +1,31 @@
 package com.deli.todoapp.model;
 
+import java.util.ArrayList;
+
 /**
  * Created by denys on 05.05.17.
  */
 public class Model {
+    private ArrayList<Record> list;
+
+    public Record addRecord()
+    {
+        Record rec = new Record();
+        list.add(rec);
+        return rec;
+    }
+
+    public Record getRecordById(int id)
+    {
+        Record rec = list.get(id);
+        return rec;
+    }
+    public void deleteRecord(Record rec)
+    {
+        list.remove(rec);
+    }
+    public void deleteRecordById(int id)
+    {
+        list.remove(id);
+    }
 }
