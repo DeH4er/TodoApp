95697ec0be6bddf7b0bb446dc2439d087024ca2a
Denys Lytviak <deh4er@gmail.com>
2017-05-05 20:00:22 +0200

    Init #2

--- src/com/deli/todoapp/Main.java
+++ src/com/deli/todoapp/Main.java
@@ -3,6 +3,6 @@ package com.deli.todoapp;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
---
+++ src/com/deli/todoapp/model/Row.java
@@ -0,0 +1,7 @@
+package com.deli.todoapp.model;
+
+/**
+ * Created by denys on 05.05.17.
+ */
+public class Row {
+}
