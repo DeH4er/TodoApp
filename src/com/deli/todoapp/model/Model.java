b3760c4f34ea1ed5b82180af0e5609e36c10af40
Denys Lytviak <deh4er@gmail.com>
2017-05-05 21:03:09 +0200

    Model refactoring

--- src/com/deli/todoapp/model/Model.java
+++ src/com/deli/todoapp/model/Model.java
@@ -1,35 +1,46 @@
 package com.deli.todoapp.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by denys on 05.05.17.
  */
 public class Model {
-    private ArrayList<Record> list;
+    private List<Record> records;
 
     public Model() {
-        list = new ArrayList<Record>();
+        records = new ArrayList<Record>();
     }
 
-    public Record addRecord(String title)
-    {
-        Record rec = new Record(title);
-        list.add(rec);
-        return rec;
+    public Record addRecord(String title) {
+        Record record = new Record(title);
+        records.add(record);
+        return record;
     }
 
-    public Record getRecordById(int id)
-    {
-        Record rec = list.get(id);
-        return rec;
+    public Record getRecordById(int id) {
+        if (id >= 0 && id < records.size()) {
+            return records.get(id);
+        } else {
+            return null;
+        }
     }
-    public void deleteRecord(Record rec)
-    {
-        list.remove(rec);
+
+    public boolean deleteRecord(Record record) {
+        if (records.contains(record)) {
+            records.remove(record);
+            return true;
+        } else {
+            return false;
+        }
     }
-    public void deleteRecordById(int id)
-    {
-        list.remove(id);
+    public boolean deleteRecordById(int id) {
+        if (id >= 0 && id < records.size()) {
+            records.remove(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
